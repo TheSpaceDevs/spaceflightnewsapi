6ee4d0ad5ded00b067af5f88249d84841d0a85e6
derk <derk@derkweijers.nl>
2018-07-14 12:31:42 +0200

    Connecing to the mongodb

--- server.js
+++ server.js
@@ -14,12 +14,16 @@ const FileStreamRotator = require('file-stream-rotator');
 const logDirectory = `${__dirname}/log`;
 
 // Config
+const mongoose = require('mongoose');
 const config = require('config');
 const routes = require('./routes');
 
 // BodyParser allows us to get data out of URLs
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Connecting to the database
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).catch(err => console.log(err));
 
 // Log all requests in a daily log file using morgan
 if (fs.existsSync(logDirectory) === false) {
