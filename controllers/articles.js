9f02088ee45d1442c22e7994e15e655abcfaee86
derk <derk@derkweijers.nl>
2018-07-14 12:24:51 +0200

    Methods to find articles

---
+++ controllers/articles.js
@@ -0,0 +1,26 @@
+const Article = require('../models/article');
+
+exports.byNewsSite = (req, res) => {
+  Article.find({ news_site: req.params.newssite }, (err, articles) => {
+    if (err) { res.send(err); }
+    if (!articles.length) {
+      res.status(404).json({ message: 'Requested news site not found' });
+    } else {
+      res.send(articles);
+    }
+  });
+};
+
+exports.findByCategory = (req, res) => {
+  Article.find({ categories: req.params.category }, (err, articles) => {
+    if (err) { res.send(err); }
+    res.send(articles);
+  });
+};
+
+exports.findByTag = (req, res) => {
+  Article.find({ tags: req.params.tag }, (err, articles) => {
+    if (err) { res.send(err); }
+    res.send(articles);
+  });
+};
