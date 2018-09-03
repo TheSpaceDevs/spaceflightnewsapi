55924ba3b694f14e4ada42fe589543f98f717a24
derk <derk@derkweijers.nl>
2018-09-03 09:20:14 +0200

    Big refactor to use req.query

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,38 +1,16 @@
 /* eslint-disable radix */
 const Article = require('../models/article');
 
-exports.latestArticles = (req, res) => {
-  Article.find({}, (err, article) => {
-    if (err) res.send(err);
+exports.articlesEndpoint = (req, res) => {
+  // set the limit to a separate variable and then remove it from the object
+  const limit = req.query.limit;
+  delete req.query.limit;
+  Article.find({ $or: [req.query] }, (err, article) => {
+    if (err) { res.send(err); }
     res.send(article);
   })
-    .limit(parseInt(req.params.limit))
+    .limit(parseInt(limit))
     .sort({ date_published: -1 });
-};
-
-exports.findByNewsSite = (req, res) => {
-  Article.find({ news_site: { $regex: req.params.newssite, $options: 'i' } }, (err, articles) => {
-    if (err) { res.send(err); }
-    if (!articles.length) {
-      res.status(404).json({ message: 'Requested news site not found' });
-    } else {
-      res.send(articles);
-    }
-  });
-};
-
-exports.findByCategory = (req, res) => {
-  Article.find({ categories: { $regex: req.params.category, $options: 'i' } }, (err, articles) => {
-    if (err) { res.send(err); }
-    res.send(articles);
-  });
-};
-
-exports.findByTag = (req, res) => {
-  Article.find({ tags: { $regex: req.params.tag, $options: 'i' } }, (err, articles) => {
-    if (err) { res.send(err); }
-    res.send(articles);
-  });
 };
 
 exports.findByID = (req, res) => {
@@ -40,22 +18,4 @@ exports.findByID = (req, res) => {
     if (err) { res.send(err); }
     res.send(article);
   });
-};
-
-exports.titleCatTags = (req, res) => {
-  Article.find({ $or: [{ title: { $regex: req.params.searchTerm, $options: 'i' } }, { categories: { $regex: req.params.searchTerm, $options: 'i' } }, { tags: { $regex: req.params.searchTerm, $options: 'i' } }] }, (err, article) => {
-    if (err) { res.send(err); }
-    res.send(article);
-  })
-    .limit(parseInt(req.params.limit))
-    .sort({ date_published: -1 });
-};
-
-exports.articlesEndpoint = (req, res) => {
-  Article.find({ $or: [req.query] }, (err, article) => {
-    if (err) { res.send(err); }
-    res.send(article);
-  })
-    .limit(parseInt(req.params.limit))
-    .sort({ date_published: -1 });
 };
