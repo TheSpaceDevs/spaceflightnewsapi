fbbbefb19342244e06ad9ba6acdf5f0701cc921d
Derk Weijers <derk@derkweijers.nl>
2018-09-04 13:44:26 +0200

    Added logging

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 
 // Logging
 const bodyParser = require('body-parser');
+let morgan = require('morgan');
+let fs = require('fs');
+let FileStreamRotator = require('file-stream-rotator');
+let logDirectory = __dirname + '/log';
 
 // Config
 const mongoose = require('mongoose');
@@ -13,6 +17,18 @@ const routes = require('./routes');
 // BodyParser allows us to get data out of URLs
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Log all requests in a daily log file using morgan
+if (fs.existsSync(logDirectory) === false) {
+  fs.mkdirSync(logDirectory);
+}
+let accessLogStream = FileStreamRotator.getStream({
+  filename: logDirectory + '/access-%DATE%.log',
+  frequency: 'daily',
+  date_format: 'YYYYMMDD',
+  verbose: false,
+});
+app.use(morgan('combined', { stream: accessLogStream }));
 
 // Connecting to the database
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).catch(err => console.log(err));
