d3e216468a0fc25ff346ae84b6548fe24bb83a55
Derk Weijers <derk@derkweijers.nl>
2018-11-10 10:02:43 +0100

    Astronauts are now queryable. Also astronaut endpoint return an object.

--- controllers/astronauts.js
+++ controllers/astronauts.js
@@ -1,13 +1,40 @@
+/* eslint-disable */
 const Astronauts = require('../models/astronaut');
 
 exports.astronautsEndpoint = (req, res) => {
-  Astronauts.find()
-    .then((astronauts) => { res.send(astronauts); });
+  const reqLimit = parseInt(req.query.limit);
+  const reqPage = parseInt(req.query.page);
+  delete req.query.limit;
+  delete req.query.page;
+  const query = Object.keys(req.query).reduce((mappedQuery, key) => {
+    const param = req.query[key];
+    if (param) {
+      mappedQuery[key] = param;
+    }
+    return mappedQuery;
+  }, {});
+
+  // Making sure we don't continue if we get page 0.
+  if (reqPage < 0 || reqPage === 0) {
+    res.json({ Error: 'Requested page can not be 0' });
+  } else {
+    Astronauts.find(query)
+      .limit(reqLimit)
+      .skip(reqLimit * (reqPage - 1))
+      .select('-status')
+      .then((astronaut) => {
+        if (astronaut === undefined || astronaut.length === 0) {
+          res.status(404).json({ Error: 'Nothing found! Please refine your search. No worries, it happens to all of us sometimes.' });
+        } else {
+          res.send(astronaut);
+        }
+      });
+  }
 };
 
 
 exports.astronautEndpoint = (req, res) => {
-  Astronauts.find(req.query)
+  Astronauts.findOne(req.query)
     .then((astronaut) => {
       if (astronaut === undefined || astronaut.length === 0) {
         res.status(404).json({ Error: 'Nothing found! Please refine your search. No worries, it happens to all of us sometimes.' });
