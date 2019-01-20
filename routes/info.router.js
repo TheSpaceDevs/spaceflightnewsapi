86f76ff2350343c7e5b24df502fbbbbd538466be
Derk Weijers <derk@derkweijers.nl>
2019-01-20 10:22:04 +0100

    Added info resource documentation

--- routes/info.router.js
+++ routes/info.router.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 const InfoController = require('../controllers/info.controller');
 
+/**
+ * Returns an object that contains general info.
+ * @route GET /info
+ * @group Info
+ * @returns {object} 200 - An object that contains general info.
+ */
 router.get('/', InfoController.getInfo);
 
 module.exports = router;
