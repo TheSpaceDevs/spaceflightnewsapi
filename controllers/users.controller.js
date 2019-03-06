2bf8ec0ee4bf62b565c0d66d56172f349391c8ae
Derk Weijers <derk@derkweijers.nl>
2019-03-06 14:01:26 +0100

    Don't need scripts for now

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -80,7 +80,7 @@ module.exports.login = async (req, res) => {
       throw new Error();
     }
 
-    const token = jwt.sign({user: user}, process.env.SECRET, {expiresIn: '1m'});
+    const token = jwt.sign({user: user}, process.env.SECRET, {expiresIn: '24h'});
 
     res.json({
       title: 'Login Successful',
