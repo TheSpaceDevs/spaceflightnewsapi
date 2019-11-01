8b1299f70fdf7ea199984880aac15e2c5ce5f3d8
Derk Weijers <derk@weijers.xyz>
2019-11-01 09:02:26 +0100

    More updates!

--- app.js
+++ app.js
@@ -28,8 +28,15 @@ app.use('/api/v1/blogs', blogsRouter);
 app.use('/api/v1/reports', reportsRouter);
 app.use('/api/v1/info', infoRouter);
 
+const mongoOptions = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  auto_reconnect: true,
+  useUnifiedTopology: true
+};
+
 try {
-  mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useCreateIndex: true, auto_reconnect: true});
+  mongoose.connect(process.env.MONGODB_URI, mongoOptions);
   mongoose.connection.on('connected', function() {
     console.log('MongoDB connected!');
   });
@@ -39,7 +46,7 @@ try {
   });
   mongoose.connection.on('disconnected', function(error) {
     console.log('MongoDB disconnected!');
-    mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useCreateIndex: true, auto_reconnect: true});
+    mongoose.connect(process.env.MONGODB_URI, mongoOptions);
   });
 } catch (e) {
   console.log(e)
