e07d8e48234f39a64e4ec234c3255365577becbf
Derk Weijers <derk@weijers.xyz>
2019-11-01 10:25:22 +0100

    Switched to Pino for logging

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
+const pino = require('pino');
+const expressPino = require('express-pino-logger');
 
 const articlesRouter = require('./routes/articles.router');
 const blogsRouter = require('./routes/blogs.router');
 const infoRouter = require('./routes/info.router');
 const reportsRouter = require('./routes/reports.router');
 
+const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
+const expressLogger = expressPino({ logger });
+
 const app = express();
 
 app.use(helmet());
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(expressLogger);
 
 app.use('/api/v1', express.static(path.join(__dirname, 'public')));
 app.use('/api/v1/articles', articlesRouter);
