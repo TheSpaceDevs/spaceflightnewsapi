425dda2f24155c3b32d1f186f51ef1757b4ba08d
Derk Weijers <derk.weijers@npo.nl>
2019-12-24 10:02:16 +0100

    Login working

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,10 +1,12 @@
 const User = require('../models/user.model');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
-const getUsers = async (req, res, next) => {
-  res.send('Hello World!')
+const getUsers = async (req, res) => {
+  return res.send('Hello World!')
 };
 
-const addUser = async (req, res, next) => {
+const addUser = async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
@@ -16,7 +18,33 @@ const addUser = async (req, res, next) => {
   }
 };
 
+const loginUser = async (req, res) => {
+  // Check if the body has the email field
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send({error: 'no email or password field in body'})
+  }
+
+  // Trying to login it has the email and password field
+  try {
+    const user = await User.findOne({email: req.body.email}).select('+password');
+    if (user) {
+      const match = await bcrypt.compare(req.body.password, user.password);
+      if (match) {
+        const token = jwt.sign({user}, process.env.SECRET, {expiresIn: '1h'});
+        return res.status(200).send({message: 'logged in', token: token});
+      } else {
+        return res.status(403).send({error: 'bad credentials'});
+      }
+    }
+    return res.status(404).send({error: 'user not found'})
+  } catch (e) {
+    res.status(500).send({error: 'server error'});
+    console.log(e)
+  }
+};
+
 module.exports = {
   getUsers,
-  addUser
+  addUser,
+  loginUser
 };
--- models/user.model.js
+++ models/user.model.js
@@ -22,7 +22,8 @@ const userSchema = new Schema({
   },
   password: {
     type: String,
-    required: true
+    required: true,
+    select: false
   },
   roles: {
     type: [String],
