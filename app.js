818486c01c28a18883f2c795e6f014e33eef3eb4
Derk Weijers <derk@weijers.xyz>
2020-06-12 08:41:54 +0200

    Remove duplicate

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(morgan('combined'));
 
