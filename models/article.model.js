1afb5d18e52282f0b9f1de5fda47e47093118ad0
Derk Weijers <derk@weijers.xyz>
2020-07-10 16:38:35 +0200

    Search implemented again

--- models/article.model.js
+++ models/article.model.js
@@ -1,73 +1,71 @@
 const mongoose = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate-v2');
 const uniqueValidator = require('mongoose-unique-validator');
-var mongoose_delete = require('mongoose-delete');
 
 const { Schema } = mongoose;
 
 const articleSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  url: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  imageUrl: {
-    type: String,
-    required: true,
-  },
-  newsSite: {
-    type: String,
-    required: true
-  },
-  summary: {
-    type: String,
-    required: false,
-    default: "No summary available"
-  },
-  publishedAt: {
-    type: Date,
-    required: true,
-  },
-  launches: [{"provider": String, "id": {}}],
-  events: [{"provider": String, "id": {}}],
-  // deleted: {
-  //   type: Boolean,
-  //   required: false,
-  //   default: false,
-  //   select: false
-  // },
-  featured: {
-    type: Boolean,
-    required: false,
-    default: false
-  }
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    url: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+    },
+    newsSite: {
+        type: String,
+        required: true,
+    },
+    summary: {
+        type: String,
+        required: false,
+        default: 'No summary available',
+    },
+    publishedAt: {
+        type: Date,
+        required: true,
+    },
+    launches: [{ 'provider': String, 'id': {} }],
+    events: [{ 'provider': String, 'id': {} }],
+    deleted: {
+      type: Boolean,
+      required: false,
+      default: false,
+      select: false
+    },
+    featured: {
+        type: Boolean,
+        required: false,
+        default: false,
+    },
 }, {
-  timestamps: true,
-  versionKey: false
+    timestamps: true,
+    versionKey: false,
 });
 
 articleSchema.index({
-  title: 'text',
-  newsSite: 'text'
+    title: 'text',
+    newsSite: 'text',
 });
 
 articleSchema.plugin(mongoosePaginate);
 articleSchema.plugin(uniqueValidator);
-articleSchema.plugin(mongoose_delete, { deletedAt : true });
 
 const ArticleModel = mongoose.model('Articles', articleSchema);
 
 ArticleModel.on('index', (err) => {
-  if (err) {
-    console.error('Article index error: %s', err);
-  } else {
-    console.info('Article indexing complete');
-  }
+    if (err) {
+        console.error('Article index error: %s', err);
+    } else {
+        console.info('Article indexing complete');
+    }
 });
 
 module.exports = ArticleModel;
