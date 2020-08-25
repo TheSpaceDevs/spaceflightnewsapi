2085df93217dab47d1c1a5ac56369bc97d5f0f0c
Derk Weijers <derk@weijers.xyz>
2020-08-25 08:16:01 +0200

    Don't sync with dev anymore and use the token

--- api/eventsync/services/eventsync.js
+++ api/eventsync/services/eventsync.js
@@ -5,26 +5,41 @@ const axios = require('axios')
  * `eventsync` service.
  */
 
+let config = {
+  headers: {
+    Authorization: `Token ${process.env.LL_TOKEN}`
+  }
+}
+
 module.exports = {
   syncLl2Events: async () => {
-    // Get the Upcoming launches
-    const upcomgingEventResult = await axios.get('https://lldev.thespacedevs.com/2.0.0/event/upcoming/?limit=100')
-    upcomgingEventResult.data.results.forEach(async event => {
-      await strapi.query('event').model.findOneAndUpdate({eventId: event.id}, {
-        name: event.name,
-        eventId: event.id,
-        provider: '5f34e4055379f026924c61cf'
-      }, {upsert: true});
-    })
+    console.log("getting upcoming events")
+    try {
+      const upcomgingEventResult = await axios.get('https://ll.thespacedevs.com/2.0.0/event/upcoming/?limit=100', config)
+      upcomgingEventResult.data.results.forEach(async event => {
+        await strapi.query('event').model.findOneAndUpdate({eventId: event.id}, {
+          name: event.name,
+          eventId: event.id,
+          provider: '5f34e4055379f026924c61cf'
+        }, {upsert: true});
+      })
+    } catch (e) {
+      console.error(`getting upcoming events failed with status: ${e.response.status}`)
+    }
 
-    // Get the previous launches
-    const previousEventResult = await axios.get('https://lldev.thespacedevs.com/2.0.0/event/previous/?limit=100')
-    previousEventResult.data.results.forEach(async event => {
-      await strapi.query('event').model.findOneAndUpdate({eventId: event.id}, {
-        name: event.name,
-        eventId: event.id,
-        provider: '5f34e4055379f026924c61cf'
-      }, {upsert: true});
-    })
+
+    console.log("getting previous events")
+    try {
+      const previousEventResult = await axios.get('https://ll.thespacedevs.com/2.0.0/event/previous/?limit=100', config)
+      previousEventResult.data.results.forEach(async event => {
+        await strapi.query('event').model.findOneAndUpdate({eventId: event.id}, {
+          name: event.name,
+          eventId: event.id,
+          provider: '5f34e4055379f026924c61cf'
+        }, {upsert: true});
+      })
+    } catch (e) {
+      console.error(`getting previous events failed with status: ${e.response.status}`)
+    }
   }
 };
--- api/launchsync/services/launchsync.js
+++ api/launchsync/services/launchsync.js
@@ -5,26 +5,41 @@ const axios = require('axios');
  * `launchsync` service.
  */
 
+let config = {
+  headers: {
+    Authorization: `Token ${process.env.LL_TOKEN}`
+  }
+}
+
 module.exports = {
   syncLl2Launches: async () => {
-    // Get the Upcoming launches
-    const upcomingLaunchResults = await axios.get('https://lldev.thespacedevs.com/2.0.0/launch/upcoming/?limit=100')
-    upcomingLaunchResults.data.results.forEach(async launch => {
-      await strapi.query('launches').model.findOneAndUpdate({launchId: launch.id}, {
-        name: launch.name,
-        launchId: launch.id,
-        provider: '5f34e4055379f026924c61cf'
-      }, {upsert: true});
-    })
+    console.log("getting upcoming launches")
+    try {
+      const upcomingLaunchResults = await axios.get('https://ll.thespacedevs.com/2.0.0/launch/upcoming/?limit=100', config)
+      upcomingLaunchResults.data.results.forEach(async launch => {
+        await strapi.query('launches').model.findOneAndUpdate({launchId: launch.id}, {
+          name: launch.name,
+          launchId: launch.id,
+          provider: '5f34e4055379f026924c61cf'
+        }, {upsert: true});
+      })
+    } catch (e) {
+      console.error(`getting upcoming launches failed with status: ${e.response.status}`)
+    }
 
     // Get the previous launches
-    const previousLaunchResults = await axios.get('https://lldev.thespacedevs.com/2.0.0/launch/previous/?limit=100')
-    previousLaunchResults.data.results.forEach(async launch => {
-      await strapi.query('launches').model.findOneAndUpdate({launchId: launch.id}, {
-        name: launch.name,
-        launchId: launch.id,
-        provider: '5f34e4055379f026924c61cf'
-      }, {upsert: true});
-    })
+    console.log("getting previous launches")
+    try {
+      const previousLaunchResults = await axios.get('https://ll.thespacedevs.com/2.0.0/launch/previous/?limit=100', config)
+      previousLaunchResults.data.results.forEach(async launch => {
+        await strapi.query('launches').model.findOneAndUpdate({launchId: launch.id}, {
+          name: launch.name,
+          launchId: launch.id,
+          provider: '5f34e4055379f026924c61cf'
+        }, {upsert: true});
+      })
+    } catch (e) {
+      console.error(`getting previous launches failed with status: ${e.response.status}`)
+    }
   }
 };
