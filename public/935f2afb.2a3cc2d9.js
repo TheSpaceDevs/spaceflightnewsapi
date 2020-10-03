8a1287abd1ad2bc6d828f2b482c24c5c7d9d47aa
derkweijers <derk@weijers.xyz>
2020-10-03 15:30:50 +0200

    Webapp files

---
+++ public/935f2afb.2a3cc2d9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{65:function(e){e.exports=JSON.parse('{"version":"current","label":"Next","isLast":true,"docsSidebars":{},"permalinkToSidebar":{}}')}}]);
