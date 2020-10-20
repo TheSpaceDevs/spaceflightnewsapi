df1dc64aa79f85753b3e6fd69f7ae72b37112aad
derkweijers <derk@weijers.xyz>
2020-10-20 20:40:41 +0200

    Final v2 updates

---
+++ public/935f2afb.28bf9bf9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{67:function(e){e.exports=JSON.parse('{"pluginId":"default","version":"current","label":"Next","isLast":true,"docsSidebars":{},"permalinkToSidebar":{}}')}}]);
