727c83fcabb8da25d97a86b20a15dee525e1972c
derkweijers <derk@weijers.xyz>
2020-12-07 12:51:26 +0100

    Package updates

---
+++ public/935f2afb.d1c10f37.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{76:function(e){e.exports=JSON.parse('{"pluginId":"default","version":"current","label":"Next","isLast":true,"docsSidebars":{},"permalinkToSidebar":{}}')}}]);
