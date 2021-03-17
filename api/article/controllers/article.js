5ced48592b66f9ea6b681efd0c7579b0339aae4f
derkweijers <derk@weijers.xyz>
2021-03-17 15:56:49 +0100

    News per event endpoint

--- api/article/controllers/article.js
+++ api/article/controllers/article.js
@@ -169,6 +169,7 @@ module.exports = {
       return cachedLaunch;
     }
 
+    // This call is _extremely_ slow
     entities = await strapi.services.article.find({
       "launches.launchId": launchId,
     });
@@ -221,6 +222,78 @@ module.exports = {
 
     // Set it in cache
     cache.set(launchId, entities, 3600);
+
+    // Finally, return the response
+    return entities;
+  },
+
+  findPerEvent: async (ctx) => {
+    const eventId = ctx.params.id;
+    let entities;
+
+    const cachedEvent = cache.get(eventId);
+
+    // Check if we have the launch in-memory and return it
+    if (cachedEvent !== undefined) {
+      if (cachedEvent.length === 0) {
+        return ctx.notFound();
+      }
+      return cachedEvent;
+    }
+
+    // This call is _extremely_ slow
+    entities = await strapi.services.article.find({
+      "events.eventId": eventId,
+    });
+
+    // The above query will always return. Handle empty array as 404
+    if (entities.length === 0) {
+      cache.set(eventId, entities, 3600);
+      return ctx.notFound();
+    }
+
+    // Build the response we want to return
+    // Use Promise.all to since it's an async map (async for the inner Promise)
+    entities = await Promise.all(
+      entities.map(async (entity) => {
+        // Create the launch and event objects
+        // Using Promise.all since it's an async map (async to wait for the result)
+        const launches = await Promise.all(
+          entity.launches.map(async (launch) => {
+            const lp = await strapi.services.provider.findOne({
+              _id: launch.provider,
+            });
+            return { id: launch.launchId, provider: lp.name };
+          })
+        );
+
+        const events = await Promise.all(
+          entity.events.map(async (event) => {
+            const ep = await strapi.services.provider.findOne({
+              _id: event.provider,
+            });
+            return { id: event.eventId, provider: ep.name };
+          })
+        );
+
+        return {
+          id: entity._id,
+          title: entity.title,
+          url: entity.url,
+          imageUrl: entity.imageUrl,
+          newsSite: entity.newsSite.name,
+          summary: entity.summary,
+          publishedAt: entity.publishedAt,
+          updatedAt: entity.updatedAt,
+          featured: entity.featured,
+          launches: launches,
+          events: events,
+        };
+      })
+    );
+
+    // Set it in cache
+    cache.set(eventId, entities, 3600);
 
     // Finally, return the response
     return entities;
