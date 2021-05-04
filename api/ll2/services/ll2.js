0562f845d845665173a9822e6ecaf6eb5a8834e8
Derk Weijers <derk@weijers.xyz>
2021-05-04 08:59:10 +0200

    inital work on v3

---
+++ api/ll2/services/ll2.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * `ll2` service.
+ */
+
+module.exports = {
+  // exampleService: (arg1, arg2) => {
+  //   return isUserOnline(arg1, arg2);
+  // }
+
+
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3b38f62a-4230-4ef4-a5fb-d6541487bf21'
+};
