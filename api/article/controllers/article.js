2476c43afedc8cc66a5a8039a574d8fd5ed17405
Derk Weijers <derk@weijers.xyz>
2021-05-13 20:10:56 +0200

    Old logic back, but cleaner

--- api/article/controllers/article.js
+++ api/article/controllers/article.js
@@ -1,8 +1,24 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.article.search(ctx.query);
+    } else {
+      entities = await strapi.services.article.find({
+        ...ctx.query,
+        _limit: ctx.query._limit || 10,
+        _sort: ctx.query._sort || "publishedAt:DESC",
+      }, ['newsSite', 'launches.provider', 'events.provider']);
+    }
+
+    return entities.map(entity => strapi.services.utils.sanitizeArticleBlog(entity));
+  },
+};
--- api/blog/controllers/blog.js
+++ api/blog/controllers/blog.js
@@ -5,4 +5,19 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.blog.search(ctx.query);
+    } else {
+      entities = await strapi.services.blog.find({
+        ...ctx.query,
+        _limit: ctx.query._limit || 10,
+        _sort: ctx.query._sort || "publishedAt:DESC",
+      }, ['newsSite', 'launches.provider', 'events.provider']);
+    }
+
+    return entities.map(entity => strapi.services.utils.sanitizeArticleBlog(entity));
+  },
+};
