e2c037e011ba0efa23c7904b0387ca2f1e558430
Derk Weijers <derk@weijers.xyz>
2021-05-13 21:38:57 +0200

    Reports endpoint

--- api/report/controllers/report.js
+++ api/report/controllers/report.js
@@ -5,4 +5,26 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.report.search(ctx.query);
+    } else {
+      entities = await strapi.services.report.find({
+        ...ctx.query,
+        _limit: ctx.query._limit || 10,
+        _sort: ctx.query._sort || "publishedAt:DESC",
+      }, ['newsSite']);
+    }
+
+    return entities.map(entity => strapi.services.utils.sanitizeEntity(entity));
+  },
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.report.findOne({ id }, ['newsSite']);
+    return strapi.services.utils.sanitizeEntity(entity);
+  },
+};
