5299380eea117783d1d3f3ef148c9f91e8aa707f
Derk Weijers <derk@weijers.xyz>
2021-05-13 20:53:16 +0200

    Cleanup

--- api/article/controllers/article.js
+++ api/article/controllers/article.js
@@ -1,5 +1,4 @@
 'use strict';
-const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
