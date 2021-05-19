1eac89438e34b7edcd69fb9ea27740aeda35678c
Derk Weijers <derk@weijers.xyz>
2021-05-19 18:51:50 +0200

    More DO url trial/error

--- config/server.js
+++ config/server.js
@@ -4,11 +4,12 @@ module.exports = ({ env }) => ({
   cron: {
     enabled: true
   },
-  url: '/',
+  url: 'https://api.spaceflightnewsapi.net/v3',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'super_secret_jwt'),
     },
+    url: '/',
     serveAdminPanel: false
   },
   ll_token: env('LL_TOKEN', 'get_this_token_via_patreon'),
