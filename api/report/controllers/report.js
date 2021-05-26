4d0c39ddfb28fcf0070080359c046a4d4c4e37e4
Derk Weijers <derk@weijers.xyz>
2021-05-26 14:09:33 +0200

    Improved handling of invalid id's in query params

--- api/report/controllers/report.js
+++ api/report/controllers/report.js
@@ -11,11 +11,20 @@ module.exports = {
     if (ctx.query._q) {
       entities = await strapi.services.report.search(ctx.query);
     } else {
-      entities = await strapi.services.report.find({
-        ...ctx.query,
-        _limit: ctx.query._limit || 10,
-        _sort: ctx.query._sort || "publishedAt:DESC",
-      }, ['newsSite']);
+      try {
+        entities = await strapi.services.report.find({
+          ...ctx.query,
+          _limit: ctx.query._limit || 10,
+          _sort: ctx.query._sort || "publishedAt:DESC",
+        }, ['newsSite']);
+      } catch (e) {
+        if (e.code === '22P02') {
+          ctx.throw(400, 'Bad Request - please take a look at your query params')
+        }
+
+        ctx.throw(500)
+      }
+
     }
 
     return entities.map(entity => strapi.services.utils.sanitizeEntity(entity));
