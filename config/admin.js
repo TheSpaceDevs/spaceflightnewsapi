3dfe33289f16555074413300cfb113ad809c91f2
Derk Weijers <derk@weijers.xyz>
2022-10-15 16:31:21 +0200

    Config fixes

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
-module.exports = ({ env }) => ({
+module.exports = ({env}) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '158ab5652085bdf2de8a2a575b34841b'),
+    secret: env('ADMIN_JWT_SECRET'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
   },
 });
