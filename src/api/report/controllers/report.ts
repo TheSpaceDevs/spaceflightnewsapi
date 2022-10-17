f43eb65e3e79256a530606b8b78ea57b597a8ec9
Derk Weijers <derk@weijers.xyz>
2022-10-17 16:32:19 +0200

    Reports endpoint

---
+++ src/api/report/controllers/report.ts
@@ -0,0 +1,7 @@
+/**
+ * report controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::report.report');
---
+++ src/api/report/routes/report.ts
@@ -0,0 +1,7 @@
+/**
+ * report router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::report.report');
