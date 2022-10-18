513bc83328ccc29dbf3a21acd9bdde953eea876b
Derk Weijers <derk@weijers.xyz>
2022-10-18 21:22:01 +0200

    Don't need the dist in the repo

--- dist/config/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = ({ env }) => ({
-    host: env('HOST', '0.0.0.0'),
-    port: env.int('PORT', 1337),
-    app: {
-        keys: env.array('APP_KEYS'),
-    },
-});
