6b27f29ce3c6a9ba8a1380fad843e15c1813d98e
Derk Weijers <derk@weijers.xyz>
2022-10-18 21:35:01 +0200

    WIP: implementing the logic

--- src/api/article/controllers/article.ts
+++ src/api/article/controllers/article.ts
@@ -4,4 +4,9 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::article.article');
+export default factories.createCoreController('api::article.article', ({ strapi }) => ({
+  async find(ctx) {
+    const { data } = await super.find(ctx);
+    ctx.body = data
+  }
+}));
