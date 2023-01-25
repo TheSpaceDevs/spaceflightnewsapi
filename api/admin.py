b3d7434dc2a266a4ed089219ce7b088bf9c8c502
Derk Weijers <derk@weijers.xyz>
2023-01-25 11:04:59 +0100

    feat(api/admin.py): Added sorting on the published_at column
    Closes #157

--- api/admin.py
+++ api/admin.py
@@ -4,20 +4,24 @@ from api.models import Article, Blog, Event, Launch, NewsSite, Provider, Report
 
 
 # Register your models here.
+# Models that need customization
 @admin.register(Article)
 @admin.register(Blog)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ("title", "news_site", "published_at", "featured")
     filter_horizontal = ["launches", "events"]
     search_fields = ["title"]
+    ordering = ("-published_at",)
 
 
 @admin.register(Report)
 class ReportAdmin(admin.ModelAdmin):
     list_display = ("title", "news_site", "published_at")
     search_fields = ["title"]
+    ordering = ("-published_at",)
 
 
+# Models that can be registered as is
 admin.site.register(Event)
 admin.site.register(Launch)
 admin.site.register(NewsSite)
