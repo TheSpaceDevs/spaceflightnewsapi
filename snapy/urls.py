12fdee08bd8fbb056f07b95b4f1a54f977673fe5
Derk Weijers <derk@weijers.xyz>
2023-02-03 09:35:47 +0100

    style: cleanup

--- snapy/urls.py
+++ snapy/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import include, re_path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
