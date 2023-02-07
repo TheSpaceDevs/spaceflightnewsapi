5346fdc193988b644bd45a7984a1d5d255f8544f
Derk Weijers <derk@weijers.xyz>
2023-02-07 14:33:44 +0100

    Clean-up

--- snapy/settings.py
+++ snapy/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", False)
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = [os.getenv("CSRF_TRUSTED_ORIGIN")]
 
 # Application definition
 
