6b6a6f56eac6a80554f8f465309fa777e437a6e3
Derk Weijers <derk@weijers.xyz>
2023-02-21 13:52:11 +0100

    Redirect when going to the root of v4

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from django.views.generic import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework import routers
 
@@ -10,11 +11,12 @@ api_router.register(r"blogs", views.BlogViewSet, basename="blogs")
 api_router.register(r"reports", views.ReportViewSet, basename="reports")
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="documentation/")),
     path("", include(api_router.urls)),
     path("schema/", SpectacularAPIView.as_view(api_version="v4"), name="schema"),
     path(
         "documentation/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
-    ),  # Temp on the v4 path to not conflict with v3.
+    ),
 ]
