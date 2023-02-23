3f52e21473ed9ed83a98ff3a2624ceb4a76c6bc1
Derk Weijers <derk@weijers.xyz>
2023-02-23 15:53:37 +0100

    news_site and launches should show up as string fields in Swagger

--- api/views/articles.py
+++ api/views/articles.py
@@ -1,11 +1,30 @@
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import OpenApiParameter, extend_schema, extend_schema_view
 from rest_framework import viewsets
 
-from api import models, serializers
+from api.models import Article
+from api.serializers import ArticleSerializer
 from api.views.filters import DocsFilter
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "news_site",
+                OpenApiTypes.STR,
+                description="Search for documents by a specific news site. Case sensitive.",
+            ),
+            OpenApiParameter(
+                "launch",
+                OpenApiTypes.UUID,
+                description="Get all documents related to a specific launch.",
+            ),
+        ]
+    ),
+)
 class ArticleViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = models.Article.objects.all()
-    serializer_class = serializers.ArticleSerializer
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
     authentication_classes = []
     filterset_class = DocsFilter
