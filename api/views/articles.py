3c559215d1666e24e44d5c81ceaf88d8d7594e9b
Arnaud Muller <arnaud.muller1308@gmail.com>
2023-04-21 10:06:48 +0200

    Trying out proper custom filters the django way

--- api/views/articles.py
+++ api/views/articles.py
@@ -10,31 +10,30 @@ from api.views.filters import DocsFilter
 
 
 class ArticleViewSet(viewsets.ReadOnlyModelViewSet):
-
     def get_queryset(self):
         articles = Article.objects.all()
 
-        title_contains_all_filters = self.request.query_params.get("title_contains_all", None)
-        title_contains_one_filters = self.request.query_params.get("title_contains_one", None)
-        summary_contains_all_filters = self.request.query_params.get("summary_contains_all", None)
-        summary_contains_one_filters = self.request.query_params.get("summary_contains_one", None)
+        # title_contains_all_filters = self.request.query_params.get("title_contains_all", None)
+        # title_contains_one_filters = self.request.query_params.get("title_contains_one", None)
+        # summary_contains_all_filters = self.request.query_params.get("summary_contains_all", None)
+        # summary_contains_one_filters = self.request.query_params.get("summary_contains_one", None)
 
-        if title_contains_all_filters:
-            title_contains_all_filters = title_contains_all_filters.split(",")
-            query = reduce(operator.and_, (Q(title__icontains=item) for item in title_contains_all_filters))
-            articles = articles.filter(query)
-        if title_contains_one_filters:
-            title_contains_one_filters = title_contains_one_filters.split(",")
-            query = reduce(operator.or_, (Q(title__icontains=item) for item in title_contains_one_filters))
-            articles = articles.filter(query)
-        if summary_contains_all_filters:
-            summary_contains_all_filters = summary_contains_all_filters.split(",")
-            query = reduce(operator.and_, (Q(summary__icontains=item) for item in summary_contains_all_filters))
-            articles = articles.filter(query)
-        if summary_contains_one_filters:
-            summary_contains_one_filters = summary_contains_one_filters.split(",")
-            query = reduce(operator.or_, (Q(summary__icontains=item) for item in summary_contains_one_filters))
-            articles = articles.filter(query)
+        # if title_contains_all_filters:
+        #     title_contains_all_filters = title_contains_all_filters.split(",")
+        #     query = reduce(operator.and_, (Q(title__icontains=item) for item in title_contains_all_filters))
+        #     articles = articles.filter(query)
+        # if title_contains_one_filters:
+        #     title_contains_one_filters = title_contains_one_filters.split(",")
+        #     query = reduce(operator.or_, (Q(title__icontains=item) for item in title_contains_one_filters))
+        #     articles = articles.filter(query)
+        # if summary_contains_all_filters:
+        #     summary_contains_all_filters = summary_contains_all_filters.split(",")
+        #     query = reduce(operator.and_, (Q(summary__icontains=item) for item in summary_contains_all_filters))
+        #     articles = articles.filter(query)
+        # if summary_contains_one_filters:
+        #     summary_contains_one_filters = summary_contains_one_filters.split(",")
+        #     query = reduce(operator.or_, (Q(summary__icontains=item) for item in summary_contains_one_filters))
+        #     articles = articles.filter(query)
 
         return articles.order_by("-published_at")
 
--- api/views/filters.py
+++ api/views/filters.py
@@ -1,17 +1,122 @@
-from django_filters import CharFilter, FilterSet, IsoDateTimeFilter, BooleanFilter, \
-    UUIDFilter, BaseInFilter, NumberFilter
+from django.db.models import Q
+from django_filters import (
+    CharFilter,
+    FilterSet,
+    IsoDateTimeFilter,
+    BooleanFilter,
+    UUIDFilter,
+    BaseInFilter,
+    NumberFilter,
+)
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 
 
 class UUIDInFilter(BaseInFilter, UUIDFilter):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 
 class NumberInFilter(BaseInFilter, NumberFilter):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 
-class CharInFilter(BaseInFilter, CharFilter):
-    pass
+class InFilterMixin:
+    def get_filter_list(self, value):
+        return [v.strip() for v in value.split(",") if v.strip()]
+
+
+class CharInFilter(InFilterMixin, BaseInFilter, CharFilter):
+    field_name = None  # set this to the name of the field to be filtered
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="my_field_contains_all",
+                type=str,
+                location=OpenApiParameter.QUERY,
+                description=f"Search for documents by the specified {field_name}s. Can be comma-separated values.",
+            )
+        ]
+    )
+    def filter(self, qs, value):
+        if not value:
+            return qs
+        values = self.get_filter_list(value)
+        q = Q(**{f"{self.field_name}__icontains": values[0]})
+        for v in values[1:]:
+            q |= Q(**{f"{self.field_name}__icontains": v})
+        return qs.filter(q)
+
+
+class NewsSiteInFilter(CharInFilter):
+    field_name = "news_site"
+
+
+class FieldContainsOneFilter(FilterSet):
+    field_name = None  # set this to the name of the field to be filtered
+    keywords = CharFilter(method="filter_keywords")
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="keywords",
+                type=str,
+                location=OpenApiParameter.QUERY,
+                description=f"A comma-separated list of keywords of which at least one must be in the {field_name}.",
+                required=False,
+            ),
+        ]
+    )
+    def filter_keywords(self, queryset, value):
+        words = value.split(",")
+        q = Q()
+        for word in words:
+            q |= Q(**{f"{self.field_name}__icontains": word.strip()})
+        return queryset.filter(q)
+
+
+class TitleContainsOneFilter(FieldContainsOneFilter):
+    field_name = "title"
+
+
+class SummaryContainsOneFilter(FieldContainsOneFilter):
+    field_name = "summary"
+
+
+class FieldContainsAllFilter(FilterSet):
+    field_name = None  # set this to the name of the field to be filtered
+
+    keywords = CharFilter(method="filter_keywords")
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="keywords",
+                type=str,
+                location=OpenApiParameter.QUERY,
+                description=f"A comma-separated list of keywords which must all be in the {field_name}.",
+                required=False,
+            ),
+        ]
+    )
+    def filter_keywords(self, queryset, value):
+        words = value.split(",")
+        q = Q()
+        for word in words:
+            q &= Q(**{f"{self.field_name}__icontains": word.strip()})
+        return queryset.filter(q)
+
+
+class TitleContainsAllFilter(FieldContainsAllFilter):
+    field_name = "title"
+
+
+class SummaryContainsAllFilter(FieldContainsAllFilter):
+    field_name = "summary"
 
 
 class DocsFilter(FilterSet):
@@ -20,74 +125,75 @@ class DocsFilter(FilterSet):
         lookup_expr="icontains",
         label="Search for all documents with a specific phrase in the title.",
     )
+    title_contains_one = TitleContainsOneFilter()
+    title_contains_all = TitleContainsAllFilter()
+    summary_contains_one = SummaryContainsOneFilter()
+    summary_contains_all = SummaryContainsAllFilter()
+    news_site = NewsSiteInFilter()
     summary_contains = CharFilter(
         field_name="summary",
         lookup_expr="icontains",
         label="Search for all documents with a specific phrase in the summary.",
     )
-    news_site = CharInFilter(
-        field_name="news_site__name",
-        label="Search for documents by the specified news sites. Can be multiple comma-separated sites. Case sensitive.",
-    )
     launch = UUIDInFilter(
         field_name="launches__launch_id",
-        lookup_expr='in',
+        lookup_expr="in",
     )
     event = NumberInFilter(
         field_name="events__event_id",
-        lookup_expr='in',
+        lookup_expr="in",
     )
     published_at__gte = IsoDateTimeFilter(
         field_name="published_at",
         lookup_expr="gte",
-        label="Get all documents published after a given ISO8601 timestamp (included)."
+        label="Get all documents published after a given ISO8601 timestamp (included).",
     )
     published_at__lte = IsoDateTimeFilter(
         field_name="published_at",
         lookup_expr="lte",
-        label="Get all documents published before a given ISO8601 timestamp (included)."
+        label="Get all documents published before a given ISO8601 timestamp (included).",
     )
     published_at__gt = IsoDateTimeFilter(
         field_name="published_at",
         lookup_expr="gt",
-        label="Get all documents published after a given ISO8601 timestamp (excluded)."
+        label="Get all documents published after a given ISO8601 timestamp (excluded).",
     )
     published_at__lt = IsoDateTimeFilter(
         field_name="published_at",
         lookup_expr="lt",
-        label="Get all documents published before a given ISO8601 timestamp (excluded)."
+        label="Get all documents published before a given ISO8601 timestamp (excluded).",
     )
     updated_at__gte = IsoDateTimeFilter(
         field_name="updated_at",
         lookup_expr="gte",
-        label="Get all documents updated after a given ISO8601 timestamp (included)."
+        label="Get all documents updated after a given ISO8601 timestamp (included).",
     )
     updated_at__lte = IsoDateTimeFilter(
         field_name="updated_at",
         lookup_expr="lte",
-        label="Get all documents updated before a given ISO8601 timestamp (included)."
+        label="Get all documents updated before a given ISO8601 timestamp (included).",
     )
     updated_at__gt = IsoDateTimeFilter(
         field_name="updated_at",
         lookup_expr="gt",
-        label="Get all documents updated after a given ISO8601 timestamp (excluded)."
+        label="Get all documents updated after a given ISO8601 timestamp (excluded).",
     )
     updated_at__lt = IsoDateTimeFilter(
         field_name="updated_at",
         lookup_expr="lt",
-        label="Get all documents updated before a given ISO8601 timestamp (excluded)."
+        label="Get all documents updated before a given ISO8601 timestamp (excluded).",
     )
     has_launch = BooleanFilter(
         field_name="launches",
         lookup_expr="isnull",
         exclude=True,
-        label="Get all documents related to a launch."
+        label="Get all documents related to a launch.",
     )
     has_event = BooleanFilter(
         field_name="events",
         lookup_expr="isnull",
         exclude=True,
-        label="Get all documents related to an event."
+        label="Get all documents related to an event.",
     )
 
 
