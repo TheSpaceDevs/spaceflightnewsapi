29891ca4e04c8ce6f5dcc5641dbe8df32c3bf9fa
Derk Weijers <derk@weijers.xyz>
2023-05-12 10:54:00 +0200

    test: added tests for the article and blog endpoints

--- api/tests/__init__.py
+++ api/tests/__init__.py
@@ -1,2 +1,4 @@
-from api.tests.article_endpoint_tests import TestArticleEndpoint
+from api.tests.article_blog_endpoint_tests import TestArticleAndBlogEndpoints
 from api.tests.info_endpoint_tests import TestInfoEndpoint
+
+__all__ = ["TestArticleAndBlogEndpoints", "TestInfoEndpoint"]
