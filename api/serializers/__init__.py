e17ca03805729d7ed71474091be2d4869c3d991c
Derk Weijers <derk@weijers.xyz>
2023-05-12 19:42:17 +0200

    fix: set the correct serializer for the info endpoint

--- api/serializers/__init__.py
+++ api/serializers/__init__.py
@@ -1,6 +1,7 @@
 from api.serializers.article_serializer import ArticleSerializer
 from api.serializers.blog_serializer import BlogSerializer
 from api.serializers.event_serializer import EventSerializer
+from api.serializers.info_serializer import InfoSerializer
 from api.serializers.launch_serializer import LaunchSerializer
 from api.serializers.news_site_serializer import NewsSiteSerializer
 from api.serializers.report_serializer import ReportSerializer
@@ -12,4 +13,5 @@ __all__ = [
     "LaunchSerializer",
     "NewsSiteSerializer",
     "ReportSerializer",
+    "InfoSerializer",
 ]
