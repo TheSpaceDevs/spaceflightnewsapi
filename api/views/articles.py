3e181115fc50afdbc05012f554a925a9170112e8
Derk Weijers <derk@weijers.xyz>
2023-05-15 11:07:09 +0200

    feat: moved ordering from the model to drf

--- api/views/articles.py
+++ api/views/articles.py
@@ -10,6 +10,12 @@ class ArticleViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     authentication_classes = []
-    filter_backends = [rest_framework.DjangoFilterBackend, filters.SearchFilter]
+    filter_backends = [
+        rest_framework.DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
     filterset_class = DocsFilter
     search_fields = ["title", "summary"]
+    ordering = ["-published_at"]
+    ordering_fields = ["published_at", " updated_at"]
