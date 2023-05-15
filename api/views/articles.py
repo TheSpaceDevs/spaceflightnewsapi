23746c47ad51d0672457e289c7920db7f25c8ad0
Derk Weijers <derk@weijers.xyz>
2023-05-15 10:50:37 +0200

    feat: search functionality that spans title and summary

--- api/views/articles.py
+++ api/views/articles.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from django_filters import rest_framework
+from rest_framework import filters, viewsets
 
 from api.models import Article
 from api.serializers import ArticleSerializer
@@ -9,4 +10,6 @@ class ArticleViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     authentication_classes = []
+    filter_backends = [rest_framework.DjangoFilterBackend, filters.SearchFilter]
     filterset_class = DocsFilter
+    search_fields = ["title", "summary"]
