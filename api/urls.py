b728d2ad1112dfae84de65e444182c3b30913d45
Derk Weijers <derk@weijers.xyz>
2023-05-16 10:13:32 +0200

    docs: fixes #207

--- api/urls.py
+++ api/urls.py
@@ -11,12 +11,12 @@ api_router.register(r"blogs", views.BlogViewSet, basename="blogs")
 api_router.register(r"reports", views.ReportViewSet, basename="reports")
 
 urlpatterns = [
-    path("", RedirectView.as_view(url="documentation/")),
+    path("", RedirectView.as_view(url="docs/")),
     path("", include(api_router.urls)),
     path("info/", views.InfoView.as_view()),
     path("schema/", SpectacularAPIView.as_view(api_version="v4"), name="schema"),
     path(
-        "documentation/",
+        "docs/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
