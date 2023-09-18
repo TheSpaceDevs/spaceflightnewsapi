15f44a90ca1e13a80b5cbd810bede1e502632787
Derk Weijers <derkweijers@users.noreply.github.com>
2023-09-18 20:50:20 +0200

    feat: added ruff and pre-commit config (#2391)
    * feat: added ruff and pre-commit config
    
    * feat: added ruff and pre-commit config
    
    * feat: added ruff and pre-commit config
    
    * feat: added ruff and pre-commit config
    
    * feat: added ruff and pre-commit config
    
    * feat: added ruff and pre-commit config
    
    * feat: ruff
    
    * feat: ruff
    
    * feat: ruff
    
    * feat: ruff
    
    * feat: ruff

--- snapy/settings.py
+++ snapy/settings.py
@@ -71,7 +71,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
