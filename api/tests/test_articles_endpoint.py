3c5293b03f19ec09e9c31f1d8ff273f0cfb00caa
Derk Weijers <derk@weijers.xyz>
2023-10-05 20:21:40 +0200

    chore: more tests

--- api/tests/test_articles_endpoint.py
+++ api/tests/test_articles_endpoint.py
@@ -1,3 +1,8 @@
+"""
+This module tests both articles and the blogs endpoints.
+Most code is shared between the two.
+"""
+
 import pytest
 
 from api.models import Article, Launch
@@ -22,9 +27,24 @@ class TestArticlesEndpoint:
         )
         assert len(data["results"]) == 10
 
-    def test_get_all_articles_with_launches(
-        self, client, articles: list[Article], launches: list[Launch]
-    ):
+    def test_get_single_article(self, client, articles: list[Article]):
+        response = client.get(f"/v4/articles/{articles[0].id}/")
+        assert response.status_code == 200
+
+        data = response.json()
+
+        assert data["title"] == articles[0].title
+        assert data["news_site"] == articles[0].news_site.name
+
+    def test_limit_articles(self, client, articles: list[Article]):
+        response = client.get("/v4/articles/?limit=5")
+        assert response.status_code == 200
+
+        data = response.json()
+
+        assert len(data["results"]) == 5
+
+    def test_get_all_articles_with_launches(self, client, articles: list[Article]):
         response = client.get("/v4/articles/?has_launch=true")
         assert response.status_code == 200
 
@@ -32,5 +52,18 @@ class TestArticlesEndpoint:
 
         assert len(data["results"]) == 2
 
-    def test_get_single_article_with_launch(self):
-        pass
+    def test_get_articles_with_launch(
+        self, client, articles: list[Article], launches: list[Launch]
+    ):
+        response = client.get(f"/v4/articles/?launch={launches[0].launch_id}")
+        assert response.status_code == 200
+
+        data = response.json()
+        assert data["results"][0]["title"] == articles[8].title
+        assert data["results"][0]["launches"][0]["launch_id"] == str(
+            launches[0].launch_id
+        )
+        assert (
+            data["results"][0]["launches"][0]["provider"] == launches[0].provider.name
+        )
+        assert len(data["results"]) == 1
