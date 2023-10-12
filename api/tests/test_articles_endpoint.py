1a39a00bc662453ba8b3e6900ea834844dadbab5
Derk Weijers <derk@weijers.xyz>
2023-10-12 11:02:30 +0200

    chore: added return types and the client type

--- api/tests/test_articles_endpoint.py
+++ api/tests/test_articles_endpoint.py
@@ -4,13 +4,14 @@ Most code is shared between the two.
 """
 
 import pytest
+from django.test.client import Client
 
 from api.models import Article, Event, Launch, NewsSite
 
 
 @pytest.mark.django_db
 class TestArticlesEndpoint:
-    def test_get_articles(self, client, articles: list[Article]):
+    def test_get_articles(self, client: Client, articles: list[Article]) -> None:
         response = client.get("/v4/articles/")
         assert response.status_code == 200
 
@@ -27,7 +28,7 @@ class TestArticlesEndpoint:
         )
         assert len(data["results"]) == 10
 
-    def test_get_single_article(self, client, articles: list[Article]):
+    def test_get_single_article(self, client: Client, articles: list[Article]) -> None:
         response = client.get(f"/v4/articles/{articles[0].id}/")
         assert response.status_code == 200
 
@@ -36,7 +37,7 @@ class TestArticlesEndpoint:
         assert data["title"] == articles[0].title
         assert data["news_site"] == articles[0].news_site.name
 
-    def test_limit_articles(self, client, articles: list[Article]):
+    def test_limit_articles(self, client: Client, articles: list[Article]) -> None:
         response = client.get("/v4/articles/?limit=5")
         assert response.status_code == 200
 
@@ -44,7 +45,9 @@ class TestArticlesEndpoint:
 
         assert len(data["results"]) == 5
 
-    def test_get_all_articles_with_launches(self, client, articles: list[Article]):
+    def test_get_all_articles_with_launches(
+        self, client: Client, articles: list[Article]
+    ) -> None:
         response = client.get("/v4/articles/?has_launch=true")
         assert response.status_code == 200
 
@@ -52,7 +55,9 @@ class TestArticlesEndpoint:
 
         assert len(data["results"]) == 2
 
-    def test_get_all_articles_with_events(self, client, articles: list[Article]):
+    def test_get_all_articles_with_events(
+        self, client: Client, articles: list[Article]
+    ) -> None:
         response = client.get("/v4/articles/?has_event=true")
         assert response.status_code == 200
 
@@ -61,8 +66,8 @@ class TestArticlesEndpoint:
         assert len(data["results"]) == 2
 
     def test_get_articles_with_launch(
-        self, client, articles: list[Article], launches: list[Launch]
-    ):
+        self, client: Client, articles: list[Article], launches: list[Launch]
+    ) -> None:
         response = client.get(f"/v4/articles/?launch={launches[0].launch_id}")
         assert response.status_code == 200
 
@@ -77,8 +82,8 @@ class TestArticlesEndpoint:
         assert len(data["results"]) == 1
 
     def test_get_articles_with_event(
-        self, client, articles: list[Article], events: list[Event]
-    ):
+        self, client: Client, articles: list[Article], events: list[Event]
+    ) -> None:
         article_with_event = [
             article for article in articles if article.title == "Article with Event 1"
         ][0]
@@ -92,8 +97,8 @@ class TestArticlesEndpoint:
         assert len(data["results"]) == 1
 
     def test_get_articles_by_news_site(
-        self, client, articles: list[Article], news_sites: list[NewsSite]
-    ):
+        self, client: Client, articles: list[Article], news_sites: list[NewsSite]
+    ) -> None:
         filtered_articles = [
             article
             for article in articles
@@ -109,8 +114,8 @@ class TestArticlesEndpoint:
         assert len(data["results"]) == len(filtered_articles)
 
     def test_get_articles_by_multiple_news_sites(
-        self, client, articles: list[Article], news_sites: list[NewsSite]
-    ):
+        self, client: Client, articles: list[Article], news_sites: list[NewsSite]
+    ) -> None:
         filtered_articles = [
             article
             for article in articles
@@ -129,7 +134,9 @@ class TestArticlesEndpoint:
             for article in data["results"]
         )
 
-    def test_get_articles_with_offset(self, client, articles: list[Article]):
+    def test_get_articles_with_offset(
+        self, client: Client, articles: list[Article]
+    ) -> None:
         response = client.get("/v4/articles/?offset=5")
         assert response.status_code == 200
 
@@ -137,7 +144,9 @@ class TestArticlesEndpoint:
 
         assert len(data["results"]) == 10
 
-    def test_get_articles_with_limit(self, client, articles: list[Article]):
+    def test_get_articles_with_limit(
+        self, client: Client, articles: list[Article]
+    ) -> None:
         response = client.get("/v4/articles/?limit=5")
         assert response.status_code == 200
 
@@ -145,7 +154,9 @@ class TestArticlesEndpoint:
 
         assert len(data["results"]) == 5
 
-    def test_get_articles_with_ordering(self, client, articles: list[Article]):
+    def test_get_articles_with_ordering(
+        self, client: Client, articles: list[Article]
+    ) -> None:
         sorted_data = sorted(
             articles, key=lambda article: article.published_at, reverse=True
         )
@@ -158,8 +169,8 @@ class TestArticlesEndpoint:
         assert data["results"][0]["title"] == sorted_data[0].title
 
     def test_get_articles_published_at_greater_then(
-        self, client, articles: list[Article]
-    ):
+        self, client: Client, articles: list[Article]
+    ) -> None:
         articles_in_the_future = list(
             filter(
                 lambda article: article.title.startswith("Article in the future"),
@@ -178,7 +189,9 @@ class TestArticlesEndpoint:
         )
         assert len(data["results"]) == 2
 
-    def test_get_articles_update_at_greater_then(self, client, articles: list[Article]):
+    def test_get_articles_update_at_greater_then(
+        self, client: Client, articles: list[Article]
+    ) -> None:
         articles_in_the_future = list(
             filter(
                 lambda article: article.title.startswith("Article in the future"),
@@ -198,8 +211,8 @@ class TestArticlesEndpoint:
         assert len(data["results"]) == 2
 
     def test_get_articles_published_at_lower_then(
-        self, client, articles: list[Article]
-    ):
+        self, client: Client, articles: list[Article]
+    ) -> None:
         articles_in_the_past = list(
             filter(
                 lambda article: article.title.startswith("Article in the past"),
@@ -218,7 +231,9 @@ class TestArticlesEndpoint:
         )
         assert len(data["results"]) == 2
 
-    def test_get_articles_update_at_lower_then(self, client, articles: list[Article]):
+    def test_get_articles_update_at_lower_then(
+        self, client: Client, articles: list[Article]
+    ) -> None:
         articles_in_the_past = list(
             filter(
                 lambda article: article.title.startswith("Article in the past"),
@@ -237,7 +252,9 @@ class TestArticlesEndpoint:
         )
         assert len(data["results"]) == 2
 
-    def test_get_article_search_articles(self, client, articles: list[Article]):
+    def test_get_article_search_articles(
+        self, client: Client, articles: list[Article]
+    ) -> None:
         response = client.get("/v4/articles/?search=title")
         assert response.status_code == 200
 
@@ -246,7 +263,9 @@ class TestArticlesEndpoint:
         assert data["results"][0]["title"] == "Article with specific title"
         assert len(data["results"]) == 1
 
-    def test_get_articles_search_summary(self, client, articles: list[Article]):
+    def test_get_articles_search_summary(
+        self, client: Client, articles: list[Article]
+    ) -> None:
         response = client.get("/v4/articles/?search=title")
         assert response.status_code == 200
 
@@ -258,7 +277,9 @@ class TestArticlesEndpoint:
         )
         assert len(data["results"]) == 1
 
-    def test_get_articles_title_contains(self, client, articles: list[Article]):
+    def test_get_articles_title_contains(
+        self, client: Client, articles: list[Article]
+    ) -> None:
         response = client.get("/v4/articles/?title_contains=Article with specific")
         assert response.status_code == 200
 
@@ -267,7 +288,9 @@ class TestArticlesEndpoint:
         assert data["results"][0]["title"] == "Article with specific title"
         assert len(data["results"]) == 1
 
-    def test_get_articles_title_contains_one(self, client, articles: list[Article]):
+    def test_get_articles_title_contains_one(
+        self, client: Client, articles: list[Article]
+    ) -> None:
         response = client.get("/v4/articles/?title_contains_one=SpaceX, specific")
         assert response.status_code == 200
 
@@ -276,7 +299,9 @@ class TestArticlesEndpoint:
         assert data["results"][0]["title"] == "Article with specific title"
         assert len(data["results"]) == 2
 
-    def test_get_articles_title_contains_all(self, client, articles: list[Article]):
+    def test_get_articles_title_contains_all(
+        self, client: Client, articles: list[Article]
+    ) -> None:
         response = client.get("/v4/articles/?title_contains_all=specific, with, title")
         assert response.status_code == 200
 
@@ -285,7 +310,9 @@ class TestArticlesEndpoint:
         assert data["results"][0]["title"] == "Article with specific title"
         assert len(data["results"]) == 1
 
-    def test_get_articles_summary_contains(self, client, articles: list[Article]):
+    def test_get_articles_summary_contains(
+        self, client: Client, articles: list[Article]
+    ) -> None:
         response = client.get("/v4/articles/?summary_contains=specific")
         assert response.status_code == 200
 
@@ -294,7 +321,9 @@ class TestArticlesEndpoint:
         assert data["results"][0]["title"] == "Article with specific title"
         assert len(data["results"]) == 1
 
-    def test_get_articles_summary_contains_one(self, client, articles: list[Article]):
+    def test_get_articles_summary_contains_one(
+        self, client: Client, articles: list[Article]
+    ) -> None:
         response = client.get("/v4/articles/?summary_contains_one=SpaceX, specific")
         assert response.status_code == 200
 
@@ -303,7 +332,9 @@ class TestArticlesEndpoint:
         assert data["results"][0]["title"] == "Article with specific title"
         assert len(data["results"]) == 2
 
-    def test_get_articles_summary_contains_all(self, client, articles: list[Article]):
+    def test_get_articles_summary_contains_all(
+        self, client: Client, articles: list[Article]
+    ) -> None:
         response = client.get(
             "/v4/articles/?summary_contains_all=specific, with, title"
         )
