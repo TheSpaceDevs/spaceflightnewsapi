ed95ed4901fa1c3b9ffd02d9610edd7c3046d66c
Derk Weijers <derk@weijers.xyz>
2023-10-17 21:16:05 +0200

    chore: rework on the serializer for mypy

--- api/serializers/launch_library/event_serializer.py
+++ api/serializers/launch_library/event_serializer.py
@@ -1,19 +1,25 @@
+from typing import TypedDict
+
 from rest_framework import serializers
 
 from api.models import Event
-from api.utils.types.event_response import EventResult
 
 
-class LaunchLibraryEventSerializer(serializers.Serializer[EventResult]):
+class ValidatedEventDataDict(TypedDict):
+    id: int
+    name: str
+
+
+class LaunchLibraryEventSerializer(serializers.Serializer[Event]):
     id = serializers.IntegerField()
     name = serializers.CharField()
 
-    def create(self, validated_data: EventResult) -> Event:
+    def create(self, validated_data: ValidatedEventDataDict) -> Event:
         event, _ = Event.objects.update_or_create(
-            event_id=validated_data.id,
+            event_id=validated_data.get("id"),
             defaults={
-                "name": validated_data.name,
-                "provider": self.context["provider"],
+                "name": validated_data.get("name"),
+                "provider": self.context.get("provider"),
             },
         )
 
--- api/serializers/launch_library/launch_serializer.py
+++ api/serializers/launch_library/launch_serializer.py
@@ -6,7 +6,7 @@ from rest_framework import serializers
 from api.models import Launch
 
 
-class ValidatedDataDict(TypedDict):
+class ValidatedLaunchDataDict(TypedDict):
     id: UUID
     name: str
 
@@ -15,8 +15,8 @@ class LaunchLibraryLaunchSerializer(serializers.Serializer[Launch]):
     id = serializers.UUIDField()
     name = serializers.CharField()
 
-    def create(self, validated_data: ValidatedDataDict) -> Launch:
-        result = Launch.objects.update_or_create(
+    def create(self, validated_data: ValidatedLaunchDataDict) -> Launch:
+        launch, _ = Launch.objects.update_or_create(
             launch_id=validated_data.get("id"),
             defaults={
                 "name": validated_data.get("name"),
@@ -24,4 +24,4 @@ class LaunchLibraryLaunchSerializer(serializers.Serializer[Launch]):
             },
         )
 
-        return result[0]
+        return launch
