1ac563dda51fd986412c89a9db232ba327259d48
Derk Weijers <derk@weijers.xyz>
2023-10-18 19:42:42 +0200

    chore: converted the v3 report serializers

--- api/serializers/v3/report_v3_serializer.py
+++ api/serializers/v3/report_v3_serializer.py
@@ -1,9 +1,22 @@
+from typing import TypedDict
+
 from rest_framework import serializers
 
 from api.models import NewsSite, Report
 
 
-class ReportV3Serializer(serializers.Serializer):
+class ValidatedReportDataDict(TypedDict):
+    id: serializers.IntegerField
+    title: serializers.CharField
+    url: serializers.URLField
+    imageUrl: serializers.URLField
+    newsSite: serializers.CharField
+    summary: serializers.CharField
+    publishedAt: serializers.DateTimeField
+    updatedAt: serializers.DateTimeField
+
+
+class ReportV3Serializer(serializers.Serializer[Report]):
     id = serializers.IntegerField()
     title = serializers.CharField()
     url = serializers.URLField()
@@ -13,19 +26,19 @@ class ReportV3Serializer(serializers.Serializer):
     publishedAt = serializers.DateTimeField()
     updatedAt = serializers.DateTimeField()
 
-    def create(self, validated_data) -> Report:
+    def create(self, validated_data: ValidatedReportDataDict) -> Report:
         news_site = NewsSite.objects.get(name=validated_data["newsSite"])
 
         report, _ = Report.objects.update_or_create(
-            id=validated_data["id"],
+            id=validated_data.get("id"),
             defaults={
-                "title": validated_data["title"],
-                "url": validated_data["url"],
-                "image_url": validated_data["imageUrl"],
+                "title": validated_data.get("title"),
+                "url": validated_data.get("url"),
+                "image_url": validated_data.get("imageUrl"),
                 "news_site": news_site,
-                "summary": validated_data["summary"],
-                "published_at": validated_data["publishedAt"],
-                "updated_at": validated_data["updatedAt"],
+                "summary": validated_data.get("summary"),
+                "published_at": validated_data.get("publishedAt"),
+                "updated_at": validated_data.get("updatedAt"),
             },
         )
 
