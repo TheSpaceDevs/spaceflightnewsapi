a2df7b4c105f71f61fbc94816d3e3191adae5fef
Derk Weijers <derk@weijers.xyz>
2023-10-19 08:07:55 +0200

    chore: ruff E501 fix

--- api/serializers/info_serializer.py
+++ api/serializers/info_serializer.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 
 
-# The type is set to None since it's not bound to a model and isn't used to deserialize data.
+# The type is set to None since it's not bound to a model
+# and isn't used to deserialize data.
 class InfoSerializer(serializers.Serializer[None]):
     version = serializers.CharField()
     news_sites = serializers.ListField(child=serializers.CharField())
