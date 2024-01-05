df76de880316c73855f3d061ffd138da2e720cdd
Derk Weijers <derk@weijers.xyz>
2024-01-05 21:42:08 +0100

    chore: cleanup

--- api/tasks/__init__.py
+++ api/tasks/__init__.py
@@ -1,10 +1,6 @@
 from api.tasks.launch_library import sync_events, sync_launches
-from api.tasks.snapi import sync_articles, sync_blogs, sync_reports
 
 __all__ = [
-    "sync_articles",
-    "sync_blogs",
-    "sync_reports",
     "sync_events",
     "sync_launches",
 ]
