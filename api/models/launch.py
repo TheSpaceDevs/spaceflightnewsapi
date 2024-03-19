11d415dbae2dedbd23e37b17a254c502fd173c24
Derk Weijers <derk@weijers.xyz>
2024-03-19 20:29:46 +0100

    feat: extend autocomplete fields

--- api/models/launch.py
+++ api/models/launch.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from django.db import models
 
 
@@ -11,3 +13,7 @@ class Launch(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+    @staticmethod
+    def autocomplete_search_fields() -> tuple[Literal["name"], Literal["launch_id"]]:
+        return ("name", "launch_id")
