9de119db45e09da07be66ec259527620e3c0aee6
Derk Weijers <derk@weijers.xyz>
2024-11-17 13:50:17 +0100

    feat: add logging configuration to settings

--- src/snapy/settings.py
+++ src/snapy/settings.py
@@ -231,3 +231,17 @@ HEALTH_CHECK = {
         "liveness-probe": ["DatabaseBackend"],
     },
 }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
