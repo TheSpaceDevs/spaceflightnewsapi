e2d620a27af48efee738f93bd20546aaafece9b7
Derk Weijers <derk@weijers.xyz>
2024-11-26 14:03:50 +0100

    feat: implement GraphQL types and queries for Article, Blog, Report, Launch, Event, and NewsSite

--- src/snapy/schema.py
+++ src/snapy/schema.py
@@ -1,92 +1,12 @@
-# type: ignore
+#  type: ignore
 
-from django.db.models.manager import BaseManager
-from graphene import UUID, Int, List, ObjectType, Schema, String
-from graphene_django import DjangoObjectType
+from graphene import ObjectType, Schema
 
-from api.models import Article, Blog, Event, Launch, NewsSite, Report
+from api.schema import Query as ApiQuery
 
 
-class ArticleType(DjangoObjectType):
-    class Meta:
-        model = Article
-        fields = ("id", "title", "url", "news_site", "published_at", "summary", "image_url", "launches", "events")
-
-
-class BlogType(DjangoObjectType):
-    class Meta:
-        model = Blog
-        fields = ("id", "title", "url", "news_site", "published_at", "summary", "image_url", "launches", "events")
-
-
-class ReportType(DjangoObjectType):
-    class Meta:
-        model = Report
-        fields = ("id", "title", "url", "news_site", "published_at", "summary", "image_url")
-
-
-class LaunchType(DjangoObjectType):
-    class Meta:
-        model = Launch
-        fields = ("id", "launch_id", "name", "provider")
-
-
-class EventType(DjangoObjectType):
-    class Meta:
-        model = Event
-        fields = ("id", "event_id", "name", "provider")
-
-
-class NewsSiteType(DjangoObjectType):
-    class Meta:
-        model = NewsSite
-        fields = ("id", "name")
-
-
-class Query(ObjectType):
-    all_articles = List(ArticleType)
-    all_blogs = List(BlogType)
-    all_reports = List(ReportType)
-
-    articles_by_news_site = List(ArticleType, news_site=String(required=True))
-    blogs_by_news_site = List(BlogType, news_site=String(required=True))
-    reports_by_news_site = List(ReportType, news_site=String(required=True))
-
-    articles_by_launch = List(ArticleType, launch_id=UUID(required=True))
-    blogs_by_launch = List(BlogType, launch_id=UUID(required=True))
-
-    articles_by_event = List(ArticleType, event_id=Int(required=True))
-    blogs_by_event = List(ArticleType, event_id=Int(required=True))
-
-    def resolve_all_articles(self, info) -> BaseManager[Article]:
-        return Article.objects.all()
-
-    def resolve_all_blogs(self, info) -> BaseManager[Blog]:
-        return Blog.objects.all()
-
-    def resolve_all_reports(self, info) -> BaseManager[Report]:
-        return Report.objects.all()
-
-    def resolve_articles_by_news_site(self, info, news_site: str) -> BaseManager[Article]:
-        return Article.objects.filter(news_site__name=news_site)
-
-    def resolve_blogs_by_news_site(self, info, news_site: str) -> BaseManager[Article]:
-        return Article.objects.filter(news_site__name=news_site)
-
-    def resolve_reports_by_news_site(self, info, news_site: str) -> BaseManager[Article]:
-        return Article.objects.filter(news_site__name=news_site)
-
-    def resolve_articles_by_launch(self, info, launch_id: UUID) -> BaseManager[Article]:
-        return Article.objects.filter(launches__launch_id=launch_id)
-
-    def resolve_blogs_by_launch(self, info, launch_id: UUID) -> BaseManager[Blog]:
-        return Blog.objects.filter(launches__launch_id=launch_id)
-
-    def resolve_articles_by_event(self, info, event_id: int) -> BaseManager[Article]:
-        return Article.objects.filter(events__event_id=event_id)
-
-    def resolve_blogs_by_event(self, info, event_id: int) -> BaseManager[Blog]:
-        return Blog.objects.filter(events__event_id=event_id)
+class Query(ApiQuery, ObjectType):
+    pass
 
 
 schema = Schema(query=Query)
