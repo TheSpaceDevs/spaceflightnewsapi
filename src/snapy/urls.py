f1e931f027f031b97d1d81a2e1c8b4f50be97d10
Derk Weijers <derk@weijers.xyz>
2024-12-02 19:04:01 +0100

    feat: move admin to non-v4 url

--- src/snapy/urls.py
+++ src/snapy/urls.py
@@ -23,11 +23,12 @@ from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView  # type: ignore
 
 urlpatterns = [
-    re_path(r"^v4/jet/", include("jet.urls", "jet")),  # Django JET URLS
     re_path(r"^v4/jet/dashboard/", include("jet.dashboard.urls", "jet-dashboard")),  # Django JET dashboard URLS
-    re_path(r"^v4/", include(("api.urls", "api"), namespace="v4")),
+    re_path(r"^v4/jet/", include("jet.urls", "jet")),  # Django JET URLS
     re_path(r"^v4/graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    re_path(r"^v4/", include(("api.urls", "api"), namespace="v4")),
     # Non v4 URLs
     re_path(r"health/", include("health_check.urls")),
+    re_path(r"admin/", admin.site.urls),
     re_path(r"admin/", admin.site.urls),
 ]
