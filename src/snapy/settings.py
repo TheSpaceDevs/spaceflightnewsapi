4fd1742b6efb8d5b346af439317ccf218d5fb501
Derk Weijers <derk@weijers.xyz>
2024-12-02 07:43:16 +0100

    fix: update STATIC_ROOT and add STATICFILES_DIRS for improved static file handling

--- src/snapy/settings.py
+++ src/snapy/settings.py
@@ -168,7 +168,9 @@ if env.bool("USE_S3", False):
     STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 else:
     STATIC_URL = "static/"
-    STATIC_ROOT = BASE_DIR / "staticfiles"
+    STATIC_ROOT = BASE_DIR.joinpath("staticfiles")
+
+STATICFILES_DIRS = [BASE_DIR.joinpath("static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
