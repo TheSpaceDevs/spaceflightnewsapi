6bcc54507da82f68720abde497cadac1dba1c1fc
Derk Weijers <derk@weijers.xyz>
2025-01-15 09:54:36 +0100

    feat: add fields for bluesky, linkedin, and mastodon to socials model

--- src/api/models/socials.py
+++ src/api/models/socials.py
@@ -5,6 +5,9 @@ class Socials(models.Model):
     x = models.URLField(blank=True)
     youtube = models.URLField(blank=True)
     instagram = models.URLField(blank=True)
+    bluesky = models.URLField(blank=True)
+    linkedin = models.URLField(blank=True)
+    mastodon = models.URLField(blank=True)
 
     class Meta:
         verbose_name_plural = "Socials"
