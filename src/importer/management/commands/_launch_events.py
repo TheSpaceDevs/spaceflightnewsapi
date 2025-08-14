d07a1458e64489734637ebb19506449f37949850
Derk Weijers <derk@weijers.xyz>
2025-08-14 13:51:43 +0200

    fix: make http errors a bit more clearer

--- src/importer/management/commands/_launch_events.py
+++ src/importer/management/commands/_launch_events.py
@@ -6,7 +6,10 @@ from django.conf import settings
 from api.models import Provider
 from importer.serializers import LaunchLibraryEventSerializer, LaunchLibraryLaunchSerializer
 
-provider = Provider.objects.get(name="Launch Library 2")
+try:
+    provider = Provider.objects.get(name="Launch Library 2")
+except Provider.DoesNotExist:
+    raise ValueError("Launch Library 2 provider does not exist. Please create it before running this command.")
 
 
 class ClientOptions(TypedDict):
@@ -29,14 +32,17 @@ def fetch_launches() -> None:
 
     with httpx.Client(base_url=client_options["base_url"], timeout=1440, headers=client_options["headers"]) as client:
         while next_url:
-            response = client.get(url=next_url).json()
+            response = client.get(url=next_url)
+            response.raise_for_status()
 
-            for data in response["results"]:
-                launch = LaunchLibraryLaunchSerializer(data=data, context={"provider": provider})
+            data = response.json()
+
+            for launch in data["results"]:
+                launch = LaunchLibraryLaunchSerializer(data=launch, context={"provider": provider})
                 launch.is_valid(raise_exception=True)
                 launch.save()
 
-            next_url = response["next"]
+            next_url = data["next"]
 
 
 def fetch_events() -> None:
@@ -44,11 +50,14 @@ def fetch_events() -> None:
 
     with httpx.Client(base_url=client_options["base_url"], timeout=1400, headers=client_options["headers"]) as client:
         while next_url:
-            response = client.get(url=next_url).json()
+            response = client.get(url=next_url)
+            response.raise_for_status()
 
-            for data in response["results"]:
-                event = LaunchLibraryEventSerializer(data=data, context={"provider": provider})
+            data = response.json()
+
+            for event in data["results"]:
+                event = LaunchLibraryEventSerializer(data=event, context={"provider": provider})
                 event.is_valid(raise_exception=True)
                 event.save()
 
-            next_url = response["next"]
+            next_url = data["next"]
