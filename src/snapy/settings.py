2b9bb68080d9d2c2de0783a739f981c6eb382f86
Derk Weijers <derkweijers@users.noreply.github.com>
2025-08-14 14:04:34 +0200

    fix: fix ll syncs (#3699)
    * fix: update the workdir
    
    * fix: static configs (#3695)
    
    * fix: add extra secrets (#3696)
    
    * fix: add static url setting
    
    * chore: fix launch sync (#3698)
    
    * fix: make http errors a bit more clearer
    
    * chore: add some logging
    
    * fix: update launch and event endpoint urls and configs

--- src/snapy/settings.py
+++ src/snapy/settings.py
@@ -184,6 +184,7 @@ STORAGES = {
 }
 
 STATICFILES_DIRS = [BASE_DIR.joinpath("static")]
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
