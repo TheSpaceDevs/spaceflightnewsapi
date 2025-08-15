34dd4bdd3e0fbea125e51d98a604b6fe996a8d3c
Derk Weijers <derkweijers@users.noreply.github.com>
2025-08-15 11:45:35 +0200

    ci: prod (#3706)
    * chore: use the x_forwarded_for header
    
    * fix: set num_proxies to be used by drf
    
    * ci: merge prod back into main
    
    * build: 4.22.0-rc.1
    
    Automatically generated by python-semantic-release
    
    * chore: use the x_forwarded_for header (#3702)
    
    * build: 4.22.0-rc.2
    
    Automatically generated by python-semantic-release
    
    * build: 4.22.0
    
    Automatically generated by python-semantic-release
    
    ---------
    
    Co-authored-by: semantic-release <semantic-release>

--- src/snapy/settings.py
+++ src/snapy/settings.py
@@ -198,6 +198,7 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_VERSIONING_CLASS": "rest_framework.versioning.NamespaceVersioning",
+    "NUM_PROXIES": env.int("NUM_PROXIES", 0),
 }
 
 
