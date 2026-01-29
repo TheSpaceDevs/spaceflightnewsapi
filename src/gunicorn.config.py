33789eddb1706e9fe6eed75a1fb79a115d7c519c
Derk Weijers <derk@weijers.xyz>
2026-01-29 16:35:07 +0100

    chore: use a single worker per pod

--- src/gunicorn.config.py
+++ src/gunicorn.config.py
@@ -1,5 +1,3 @@
-from multiprocessing import cpu_count
-
 from environs import Env
 
 env = Env()
@@ -7,8 +5,8 @@ env.read_env()
 
 bind = ":8000"
 
-# https://gunicorn.org/guides/docker/#running-in-background
-workers = (2 * cpu_count()) + 1
+# https://blog.graywind.org/posts/gunicorn-in-containers/
+workers = 1
 
 errorlog = "-"
 loglevel = "info"
